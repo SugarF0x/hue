89c9ccf54c29914ab6b798db2e0bf2d2f29f80ed
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-01-01 22:52:55 +0300

    install ts & nodemon

---
+++ shim.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    MY_VARIABLE_1: string;
+    MY_OTHER_VARIABLE: string;
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { Bridge, Hue } from 'hue'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const { HUE_BRIDGE, HUE_USER } = process.env
+
+async function getBridge(): Promise<Bridge> {
+  const hue = new Hue(HUE_BRIDGE, HUE_USER)
+
+  return new Promise(resolve => {
+    hue.on('ready', resolve)
+  })
+}
+
+async function main() {
+  const bridge = await getBridge()
+  console.log(bridge)
+}
+
+void main()
