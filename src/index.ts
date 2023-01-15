2184fb240db7e18d89434a7b375f86a73f38641f
Alexey Tuzov <sugarfox.dev@gmail.com>
2023-01-15 12:30:15 +0300

    add living room controls

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,28 @@ async function getBridge(): Promise<Bridge> {
   })
 }
 
+async function delay(ms: number) {
+  await new Promise(resolve => setTimeout(resolve, ms))
+}
+
 async function main() {
   const bridge = await getBridge()
-  const lights = await bridge.Light.all()
+  const groups = await bridge.Group.all()
+  const livingRoomLightIds = groups.find(group => group.name.toLowerCase().includes('living'))?.lights ?? []
+  const allLights = await bridge.Light.all()
+  const lights = allLights.filter(light => livingRoomLightIds.includes(light.id))
+
   console.log(lights)
+
+  for (const light of lights) {
+    await light.on()
+    await delay(500)
+  }
+
+  for (const light of lights) {
+    await light.off()
+    await delay(500)
+  }
 }
 
 void main()
